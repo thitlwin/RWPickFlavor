4df791217ee897fa4e6b6e098c6c0364be99ea8b
orlando <jsdcorlando@itcom888.com>
2024-11-09 11:17:38 +0700

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,18 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by thit on 11/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
---
+++ RWPickFlavorTests/RWPickFlavorTests.swift
@@ -0,0 +1,17 @@
+//
+//  RWPickFlavorTests.swift
+//  RWPickFlavorTests
+//
+//  Created by thit on 11/9/24.
+//
+
+import Testing
+@testable import RWPickFlavor
+
+struct RWPickFlavorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
